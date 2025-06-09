76c2a63c264bb4bc602c892f4382374dc72c5e39
Ryan Forgie <ryan.j.forgie@gmail.com>
2025-06-09 21:19:15 +1200

    add photo background to left panel

---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="astro/client" />
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+} 
