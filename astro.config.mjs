6163045fee61b1d36ab1e351ad0d6d906be5ada2
Ryan Forgie <ryan.j.forgie@gmail.com>
2025-07-14 19:57:38 +1200

    performance optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +8,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()]
+  integrations: []
 });
