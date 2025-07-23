d431640876c81ce3b0feca728c21bfac19cf4af3
Ryan Forgie <ryan.j.forgie@gmail.com>
2025-07-23 19:01:04 +1200

    add security headers and correct production url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'http://www.gravel.nz',
+  site: 'https://www.gravel.nz',
   vite: {
     plugins: [tailwindcss()],
   },
